33840e54b500704f8f8a9f378695ac1387b98e01
topher1794 <cristopher.evangelista1794@gmail.com>
2020-05-16 09:17:13 +0800

    add

--- source/conf.py
+++ source/conf.py
@@ -53,11 +53,15 @@ html_theme = 'default'
 
 html_theme_options = {
     'navigation_depth': 4,
-}
+}  
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
 
 master_doc = 'index'
